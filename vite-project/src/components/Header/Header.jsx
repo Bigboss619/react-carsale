c904975e92b4e5bf4b1c671930046596c6c07a91
Bigboss619$ <eugochukwu910@gmail.com>
2026-02-06 09:31:07 +0100

    `Update Header component: change condition in login/register button section`

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -86,7 +86,7 @@ const navItems = [
                 {/* Login/Register Button */}
                 <div className='hidden md:flex items-center space-x-4' id="login-register-section">
                     {loading ? null : (
-                        !user ? (
+                        !user || !role ? (
                             <>
                                 {/* Guest */}
                                 <Link to="/login" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors duration-200 font-bold">
